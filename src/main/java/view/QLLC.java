db626eb904dfc2b22630693e80f1d83b5dfd2967
NhatTrung-Git <nhattrung.kg@gmail.com>
2021-06-10 18:15:11 +0700

    GUITrung QLLC reup

--- src/main/java/view/QLLC.java
+++ src/main/java/view/QLLC.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUIandBUS;
+package view;
 
 import dao.ConnectionUtils;
 import java.awt.Color;
